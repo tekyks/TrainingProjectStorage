73256b73152ecddbc35a263382e64582379eb0f9
Krishna Srivastava <teky.ks@gmail.com>
2025-08-05 14:50:12 +0530

    CAPM Project - Functions and Actions complete - Day 16

--- capmproject2/srv/main.cds
+++ capmproject2/srv/main.cds
@@ -12,16 +12,22 @@ service bookshop {
     //         title = 'Deep Work';
 
 
-    entity Books   as projection on org.tekyks.Books;
+    //entity Books   as projection on org.tekyks.Books;
+    entity Books   as projection on org.tekyks.Books
+        actions {
+            //bound function
+            function stockValue()              returns Integer;
+            //bound action
+            action   setPrice(price : Integer) returns Books;
+        };
 
-    
 
     entity Authors as projection on org.tekyks.Authors;
     entity Orders  as projection on org.tekyks.Orders;
-
     //unbound function
-    function getAllBooksTitle() returns String;
-
+    function getAllBooksTitle()                          returns String;
     //unbound function
-    function totalStock() returns Integer;
+    function totalStock()                                returns Integer;
+    //unbound action
+    action   submitBookOrder(id : UUID, stock : Integer) returns Integer;
 }
---
+++ capmproject2/srv/main2.js
@@ -0,0 +1,120 @@
+//console.log("I am inside JS File");
+
+// module.exports = function(){
+//     console.log("I am inside anonymous function");
+// };
+
+const cds = require('@sap/cds');
+module.exports = cds.service.impl(function () {
+  //console.log("I am inside cds service impl function");
+  // this.on('READ','Books',function(){
+  //     console.log("I am inside Book Read function");
+  // });
+
+  this.on('READ', 'Books', function (req, next) {
+    console.log('I am inside Book Read function');
+    //console.log(req);
+    //next() method will do further operations
+    return next();
+    // return[
+    //     {
+    //         ID:cds.utils.uuid(),
+    //         title:'This is Custom Title',
+    //         author_ID: '2'
+    //     },
+    //     {
+    //         ID:cds.utils.uuid(),
+    //         title:'This is Custom Title 2',
+    //         author_ID: '3'
+    //     }
+    // ];
+  });
+
+  const doModification = function (data) {
+    return data.map((data) => {
+      //data.title += "!!!!!!!!!";
+      if (data.title.toLowerCase().includes('work')) {
+        data.category = 'Business Book';
+      } else {
+        data.category = 'Fiction Book';
+      }
+    });
+  };
+
+  this.after('READ', 'Books', function (data, req) {
+    console.log('I am inside after impl of READ');
+    console.log(data);
+    doModification(data);
+  });
+
+  //unbound function
+  this.on('getAllBooksTitle', function () {
+    return 'List of Titles';
+  });
+
+  //decalre Books variable
+  const Books = cds.entities('bookshop')['Books'];
+  //1- unbound function
+  this.on('totalStock', async function () {
+    //get the total stock by fetching all the books
+    //stock field and adding all
+    //1- get all the books
+    //2- get only stock field
+    //3- do the summation of all the stocks
+    //4- (optional) give alias name to computed column
+
+    const result = await SELECT.one
+      .columns('sum(stock) as totalstock')
+      .from(Books);
+
+    return result.totalstock;
+  });
+
+  //2- bound function
+  //1- we will get stock and price from Book where Book Id should be passed value
+  //2- once we get price and stock, we will multiply
+  this.on('stockValue', 'Books', async function (request) {
+    //console.log(request.params);
+    const id = request.params[0];
+    const result = await SELECT.one
+      .columns('stock*price as stockValue')
+      .from(Books).where`ID=${id}`;
+
+    return result.stockValue;
+
+    //return 77;
+  });
+
+  //3- Unbound Action
+  this.on('submitBookOrder', async function (request) {
+    const id = request.data.id;
+    const stock = request.data.stock;
+    const stockObject = await SELECT.columns('stock').from(Books, id);
+    const currentStock = stockObject.stock;
+    const updatedStock = currentStock - stock;
+    await UPDATE(Books, id).with({
+      stock: updatedStock,
+    });
+
+    return updatedStock;
+  });
+
+  //4- Bound Action
+  //Set the price of a particular book
+  this.on('setPrice', 'Books', async function (request) {
+    //console.log(request.data);
+    const id = request.params[0];
+    const updatedPrice = request.data.price;
+    //this will update Books with updated price
+    await UPDATE(Books, id).with({
+      price: updatedPrice,
+    });
+
+    return await SELECT.from(Books, id);
+  });
+
+  // this.after('each', 'Books', function(data,req){
+  //     console.log("I am inside after impl of READ");
+  //     console.log(data);
+  // });
+});
