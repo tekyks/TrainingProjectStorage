86a21ceb4d9dbf09d8c2c42ad5b7be6e0e0f06a3
tekyks @ <teky.ks@gmail.com>
2025-08-14 07:25:32 +0000

    Final CAPM Project - Day21

--- capmwithnorthwind/srv/northwind-service.js
+++ capmwithnorthwind/srv/northwind-service.js
@@ -2,5 +2,8 @@ const cds = require('@sap/cds');
 
 module.exports = cds.service.impl(async function() {
     const service = await cds.connect.to('Northwind');
+    this.on('READ', 'Products', function(request){
+        return service.run(request.query);
+    });
 
 });
